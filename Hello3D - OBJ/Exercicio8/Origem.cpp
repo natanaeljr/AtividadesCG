6fdb5671f15633779413d20a19f613d558d838c9
Natanael J. Rabello <natanaeljrabello@gmail.com>
2023-05-27 20:52:10 -0300

    Hello3D OBJ done - Geometry
    Signed-off-by: Natanael J. Rabello <natanaeljrabello@gmail.com>

--- Hello3D - OBJ/Exercicio8/Origem.cpp
+++ Hello3D - OBJ/Exercicio8/Origem.cpp
@@ -10,8 +10,10 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
 #include <assert.h>
 #include <optional>
+#include <vector>
 
 using namespace std;
 
@@ -32,10 +34,10 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 
 // Protótipos das funções
 int setupShader();
-int setupGeometry();
+int setupGeometry(const struct Obj& obj);
 
 // Dimensões da janela (pode ser alterado em tempo de execução)
-const GLuint WIDTH = 1000, HEIGHT = 1000;
+const GLuint WIDTH = 720, HEIGHT = 720;
 
 // Código fonte do Vertex Shader (em GLSL): ainda hardcoded
 const GLchar* vertexShaderSource = "#version 330\n"
@@ -62,15 +64,56 @@ const GLchar* fragmentShaderSource = "#version 330\n"
 bool rotateX=false, rotateY=false, rotateZ=false;
 
 struct Obj {
+    std::vector<glm::vec3> vertices;
+    std::vector<glm::vec3> normals;
+    std::vector<glm::vec2> texcoords;
+    std::vector<glm::u32vec3> triangle_indices;
 };
 
 auto parse_obj(const std::string& filename) -> std::optional<Obj>
 {
-    auto file = read_file_to_string(filename);
-    if (file)
+    auto file = std::ifstream(filename);
+    if (!file.good()) {
+        cerr << "Error opening OBJ file '" << filename
+             << "', error: " << std::strerror(errno) << endl;
+    }
 
-    printf("OBJ dump:\n%s", file->c_str());
-    return std::nullopt;
+    Obj obj;
+    char buf[BUFSIZ];
+    while (file.getline(buf, sizeof(buf))) {
+        std::stringstream ss(buf);
+        std::string code;
+        ss >> code;
+        if (code == "v") {
+            glm::vec3 v;
+            ss >> v.x; ss >> v.y; ss >> v.z;
+            obj.vertices.push_back(v);
+        }
+        else if (code == "vn") {
+            glm::vec3 vn;
+            ss >> vn.x; ss >> vn.y; ss >> vn.z;
+            obj.normals.push_back(vn);
+        }
+        else if (code == "vt") {
+            glm::vec2 vt;
+            ss >> vt.x; ss >> vt.y;
+            obj.texcoords.push_back(vt);
+        }
+        else if (code == "f") {
+            uint32_t _; // dummy var
+            glm::u32vec3 fv; // get only vertex indices for now
+            ss >> fv[0]; ss.get() /*slash*/; ss >> _; ss.get() /*slash*/; ss >> _;
+            ss >> fv[1]; ss.get() /*slash*/; ss >> _; ss.get() /*slash*/; ss >> _;
+            ss >> fv[2]; ss.get() /*slash*/; ss >> _; ss.get() /*slash*/; ss >> _;
+            fv -= glm::u32vec3(1); /* index is offset by 1 */
+            obj.triangle_indices.push_back(fv);
+        }
+    }
+
+    printf("Parsed OBJ file '%s' with %zu vertices, %zu normals, %zu texcoords, %zu triangles\n",
+           filename.c_str(), obj.vertices.size(), obj.normals.size(), obj.texcoords.size(), obj.triangle_indices.size());
+
+    return obj;
 }
 
 // Função MAIN
@@ -113,7 +156,12 @@ int main()
     cout << "OpenGL version supported " << version << endl;
 
     // Carrega o OBJ
-    auto obj = parse_obj("../../3D_Models/Suzanne/suzanneTri.obj");
+    auto obj = parse_obj("../../3D_Models/Suzanne/suzanneTriLowPoly.obj");
+    //auto obj = parse_obj("../../3D_Models/Cube/cube.obj");
+    if (!obj) {
+        cerr << "Failed to load 3D Model" << endl;
+        return 1;
+    }
 
     // Definindo as dimensões da viewport com as mesmas dimensões da janela da aplicação
     int width, height;
@@ -125,7 +173,7 @@ int main()
     GLuint shaderID = setupShader();
 
     // Gerando um buffer simples, com a geometria de um triângulo
-    GLuint VAO = setupGeometry();
+    GLuint VAO = setupGeometry(*obj);
 
 
     glUseProgram(shaderID);
@@ -146,10 +194,10 @@ int main()
         glfwPollEvents();
 
         // Limpa o buffer de cor
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f); //cor de fundo
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f); //cor de fundo
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // glLineWidth(10);
+         glLineWidth(2);
         // glPointSize(20);
 
         float angle = (GLfloat)glfwGetTime();
@@ -170,13 +218,25 @@ int main()
             model = glm::rotate(model, angle, glm::vec3(0.0f, 0.0f, 1.0f));
 
         }
+        model = glm::scale(model, glm::vec3(0.6f));
 
         glUniformMatrix4fv(modelLoc, 1, false, glm::value_ptr(model));
         // Chamada de desenho - drawcall
         // Poligono Preenchido - GL_TRIANGLES
 
+        // Draw the faces
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glVertexAttrib3f(1, 1.f, 0.f, 0.f); // default color for attribute color
+        glDrawElements(GL_TRIANGLES, obj->triangle_indices.size() * 3, GL_UNSIGNED_INT, 0);
+
+        // Draw the LINES
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        glBindVertexArray(VAO);
+        glVertexAttrib3f(1, 0.f, 0.f, 0.f); // default color for attribute color
+        glDrawElements(GL_TRIANGLES, obj->triangle_indices.size() * 3, GL_UNSIGNED_INT, 0);
+
+        //glDrawArrays(GL_TRIANGLES, 0, obj->vertices.size());
 
         // Chamada de desenho - drawcall
         // CONTORNO - GL_LINE_LOOP
@@ -266,85 +326,23 @@ int setupShader()
     return shaderProgram;
 }
 
-// Esta função está bastante harcoded - objetivo é criar os buffers que armazenam a
-// geometria de um triângulo
-// Apenas atributo coordenada nos vértices
-// 1 VBO com as coordenadas, VAO com apenas 1 ponteiro para atributo
-// A função retorna o identificador do VAO
-int setupGeometry()
+int setupGeometry(const struct Obj& obj)
 {
-    // Aqui setamos as coordenadas x, y e z do triângulo e as armazenamos de forma
-    // sequencial, já visando mandar para o VBO (Vertex Buffer Objects)
-    // Cada atributo do vértice (coordenada, cores, coordenadas de textura, normal, etc)
-    // Pode ser arazenado em um VBO único ou em VBOs separados
-    GLfloat vertices[] = {
-
-        //Base da pirâmide: 2 triângulos
-        //x    y    z    r    g    b
-        -0.5, -0.5, -0.5, 0.0, 1.0, 0.0,
-        -0.5, -0.5,  0.5, 0.0, 1.0, 0.0,
-         0.5, -0.5, -0.5, 0.0, 1.0, 0.0,
-
-        -0.5, -0.5, 0.5,  0.0, 1.0, 0.0,
-         0.5, -0.5,  0.5, 0.0, 1.0, 0.0,
-         0.5, -0.5, -0.5, 0.0, 1.0, 0.0,
-
-        // Esquerda
-        -0.5, -0.5, -0.5, 1.0, 0.0, 0.0,
-        -0.5,  0.5, -0.5, 1.0, 0.0, 0.0,
-        -0.5,  0.5,  0.5, 1.0, 0.0, 0.0,
-
-        -0.5, -0.5,  0.5, 1.0, 0.0, 0.0,
-        -0.5,  0.5,  0.5, 1.0, 0.0, 0.0,
-        -0.5, -0.5, -0.5, 1.0, 0.0, 0.0,
-
-        // Direita
-         0.5, -0.5, -0.5, 1.0, 0.0, 1.0,
-         0.5,  0.5, -0.5, 1.0, 0.0, 1.0,
-         0.5,  0.5,  0.5, 1.0, 0.0, 1.0,
-
-         0.5, -0.5, -0.5, 1.0, 0.0, 1.0,
-         0.5,  0.5,  0.5, 1.0, 0.0, 1.0,
-         0.5, -0.5,  0.5, 1.0, 0.0, 1.0,
-
-        // Trás
-        -0.5, -0.5, -0.5, 1.0, 1.0, 0.0,
-        -0.5,  0.5, -0.5, 1.0, 1.0, 0.0,
-         0.5,  0.5, -0.5, 1.0, 1.0, 0.0,
-
-        -0.5, -0.5, -0.5, 1.0, 1.0, 0.0,
-         0.5,  0.5, -0.5, 1.0, 1.0, 0.0,
-         0.5, -0.5, -0.5, 1.0, 1.0, 0.0,
-
-        // Frente
-        -0.5, -0.5,  0.5, 0.0, 1.0, 1.0,
-        -0.5,  0.5,  0.5, 0.0, 1.0, 1.0,
-         0.5,  0.5,  0.5, 0.0, 1.0, 1.0,
-
-        -0.5, -0.5,  0.5, 0.0, 1.0, 1.0,
-         0.5,  0.5,  0.5, 0.0, 1.0, 1.0,
-         0.5, -0.5,  0.5, 0.0, 1.0, 1.0,
-
-        // Cima
-        -0.5,  0.5, -0.5, 0.0, 0.0, 1.0,
-        -0.5,  0.5,  0.5, 0.0, 0.0, 1.0,
-         0.5,  0.5, -0.5, 0.0, 0.0, 1.0,
-
-        -0.5,  0.5, 0.5,  0.0, 0.0, 1.0,
-         0.5,  0.5,  0.5, 0.0, 0.0, 1.0,
-         0.5,  0.5, -0.5, 0.0, 0.0, 1.0,
-    };
-
-    GLuint VBO, VAO;
+    GLuint VBO, EBO, VAO;
 
     //Geração do identificador do VBO
     glGenBuffers(1, &VBO);
+
+    //Geração do identificador do EBO
+    glGenBuffers(1, &EBO);
 
     //Faz a conexão (vincula) do buffer como um buffer de array
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
     //Envia os dados do array de floats para o buffer da OpenGl
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    size_t vertex_stride_size = sizeof(decltype(obj.vertices)::value_type);
+    size_t vertex_total_size = obj.vertices.size() * vertex_stride_size;
+    glBufferData(GL_ARRAY_BUFFER, vertex_total_size, obj.vertices.data(), GL_STATIC_DRAW);
 
     //Geração do identificador do VAO (Vertex Array Object)
     glGenVertexArrays(1, &VAO);
@@ -362,18 +360,29 @@ int setupGeometry()
     // Deslocamento a partir do byte zero
 
     //Atributo posição (x, y, z)
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, vertex_stride_size, (GLvoid*)0);
     glEnableVertexAttribArray(0);
 
     //Atributo cor (r, g, b)
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3*sizeof(GLfloat)));
-    glEnableVertexAttribArray(1);
+    //glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3*sizeof(GLfloat)));
+    //glEnableVertexAttribArray(1);
 
+    // Default value for color attribute
+    glVertexAttrib3f(1, 1.f, 0.f, 0.f);
+    glDisableVertexAttribArray(1);
+
+    // Carrega os indices
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    size_t index_stride_size = sizeof(decltype(obj.triangle_indices)::value_type);
+    size_t index_total_size = obj.triangle_indices.size() * index_stride_size;
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, index_total_size, obj.triangle_indices.data(), GL_STATIC_DRAW);
 
 
     // Observe que isso é permitido, a chamada para glVertexAttribPointer registrou o VBO como o objeto de buffer de vértice
     // atualmente vinculado - para que depois possamos desvincular com segurança
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    //glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     // Desvincula o VAO (é uma boa prática desvincular qualquer buffer ou array para evitar bugs medonhos)
     glBindVertexArray(0);
--- RESULT.md
+++ RESULT.md
@@ -10,3 +10,12 @@ Diretório: [Hello3D](Hello3D)
 Diretório: [Hello3D - Cube](./Hello3D%20-%20Cube)
  
 ![Hello3DCube](extras/hello3d-cube.gif)
+
+
+## Tarefa M2 (Atividade Vivencial 1):
+
+Vizualizador 3D
+
+Diretório: [Hello3D - OBJ](./Hello3D%20-%20OBJ)
+ 
+![Hello3DObj](extras/hello3d-obj.gif)
